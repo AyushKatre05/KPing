9d9ed45552c2062e6ef00e2cfede37a4e5bdcf57
Ayush Katre <ayushkatre1801@gmail.com>
2026-03-12 20:04:17 +0530

    gradle project initialise with some stuff ongoing

---
+++ backend/build.gradle.kts
@@ -0,0 +1,54 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikari_version: String by project
+val coroutines_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.8"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+}
+
+group = "com.kping"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.kping.ApplicationKt")
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.8")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-cors-jvm:2.3.8")
+    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.8")
+    
+    // Database
+    implementation("org.jetbrains.exposed:exposed-core:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.47.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.47.0")
+    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    
+    // HTTP Client for monitoring
+    implementation("io.ktor:ktor-client-core:2.3.8")
+    implementation("io.ktor:ktor-client-cio:2.3.8")
+
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
+}
